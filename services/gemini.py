513abbc97044fdf496f826c8401aeb37b3e31bf2
seoeunbae <baeseoeun1221@gmail.com>
2025-09-26 11:00:15 +0900

    refactor : change gemini video understanding

--- services/gemini.py
+++ services/gemini.py
@@ -11,29 +11,40 @@ def generate(file_uri, prompt, text):
       project=os.getenv('GCP_PROJECT'),
       location="global",
   )
-  video_meta = types.VideoMetadata(
-      start_offset="0.0s",  # 예: 0초에서 시작
-      end_offset="10.0s", # 예: 10.5초에서 끝
-      fps=1.0             # 예: 초당 1 프레임
-  )
-  msg_text = types.Part.from_text(text=text)
-  msg_video = types.Part.from_uri(
+  msg_text = types.Part(text=text)
+  msg_video = types.FileData(
       file_uri=file_uri,
-      mime_type="video/*",
-      videoMetadata=video_meta 
+      mime_type="video/*"
   )
-  msg_prompt= types.Part.from_text(text=prompt)
-  
-  model = "gemini-2.5-pro"
-  contents = [
-      types.Content(role="user", parts=[
-        msg_video,
-        msg_text,
-        msg_prompt
-        ]
-      )
-  ]
 
+  video_meta=types.VideoMetadata(
+    start_offset="0.0s",
+    end_offset="10.0s",
+    fps=1.0
+  )
+  
+  msg_prompt= types.Part(text=prompt)
+
+  part_vidoe_content = types.Part(
+    file_data=msg_video,
+    video_metadata=video_meta,
+  )
+  model = "gemini-2.5-pro"
+  # contents = [
+  #     types.Content(role="user", parts=[
+  #       msg_video,
+  #       msg_text,
+  #       msg_prompt
+  #       ]
+  #     )
+  # ]
+  contents = types.Content(
+    parts=[
+      part_vidoe_content,
+      msg_text,
+      msg_prompt
+    ]
+  )
   generate_content_config = types.GenerateContentConfig(
     temperature = 1,
     top_p = 0.95,
